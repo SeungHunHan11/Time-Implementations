12c1a358dd5e6f31c4f12b2a887ca1c6d59d04ef
Seung Hun Han <andrewhan@korea.ac.kr>
2023-04-08 21:21:50 +0900

    Update factory.py

--- Anomaly_Transformer/datasets/factory.py
+++ Anomaly_Transformer/datasets/factory.py
@@ -31,19 +31,18 @@ class anomaly_dataset(Dataset):
         self.label = label
 
         if mode == 'train':
-            self.data = self.train_data
-            self.length = (self.train_data.shape[0] - self.window_size) // self.step_size + 1
+            self.data = self.train_data[:len(self.train_data)*0.8]
+            self.length = (self.data.shape[0] - self.window_size) // self.step_size + 1
         elif mode == 'val':
-            self.data = self.test_data
-            self.length = (self.test_data.shape[0] - self.window_size) // self.step_size + 1
-
+            self.data = self.train_data[len(self.train_data)*0.8:]
+            self.length = (self.data.shape[0] - self.window_size) // self.step_size + 1
         elif mode == 'test':
             self.data = self.test_data
-            self.length = (self.test_data.shape[0] - self.window_size) // self.step_size + 1
+            self.length = (self.data.shape[0] - self.window_size) // self.step_size + 1
 
         elif mode == 'threshold':
-            self.data = self.test_data
-            self.length = (self.test_data.shape[0] - self.window_size) // self.window_size + 1
+            self.data = self.train_data[len(self.train_data)*0.8:]
+            self.length = (self.data.shape[0] - self.window_size) // self.window_size + 1
 
         self.mode = mode
 
