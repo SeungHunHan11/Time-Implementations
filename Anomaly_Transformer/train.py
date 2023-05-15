c2f19a3a2f0dce26b6863c843bad42fbd699e106
Seung Hun Han <andrewhan@korea.ac.kr>
2023-05-16 00:06:52 +0900

    Update train.py

--- Anomaly_Transformer/train.py
+++ Anomaly_Transformer/train.py
@@ -82,12 +82,13 @@ def eval(data_loader, model, criterion, device, Lambda):
 def fit(train_loader, val_loader, model, 
         optimizer, criterion, scheduler, 
         device, Lambda, use_wandb,
-        save_dir, epochs, temperature = 50, anomaly_ratio = 4.00, 
-        threshold_loader = None, test_loader = None
+        save_dir, epochs, temperature = 50, anomaly_ratio = 4.00, threshold_loader = None,
+        point_adjustment = False
         ):
     
     best_loss_min = 0
     best_loss_max = 0
+    best_f1 = 0
 
     for epoch in range(epochs):
         train_loss_min, train_loss_max = train(train_loader, model, optimizer, criterion, device, Lambda)
@@ -129,46 +130,45 @@ def fit(train_loader, val_loader, model,
                                         anomaly_ratio = anomaly_ratio,
                                         train_energy = train_energy
                                         )
-
-                pred, ground_truth= evaluation(
-                                            loader = threshold_loader,
+                
+                anomaly_score, pred, ground_truth= evaluation(
+                                            threshold_loader = threshold_loader,
                                             device = device,
                                             criterion = criterion_infer,
                                             model = model, 
                                             temperature = temperature,
-                                            threshold = threshold
+                                            threshold = threshold,
+                                            point_adjustment = point_adjustment
                                             )
+
 
             accuracy = accuracy_score(ground_truth, pred)
             precision, recall, f_score, support = precision_recall_fscore_support(ground_truth, pred,
                                                                                 average='binary')
-            f1_sc = f1_score(ground_truth, pred)
-
+            
             eval_result = {
                         'Epoch' : epoch,
                         'test_Accuracy' : accuracy, 
                         'test_recall' : recall,
-                        'test_fscore' : f_score,
-                        'test_f1_score': f1_sc,
+                        'test_f1_score' : f_score,
                         'test_precision' : precision
                         }
 
             print(
-                "Accuracy : {:0.4f}, Precision : {:0.4f}, Recall : {:0.4f}, F-score : {:0.4f}, F1-Score {:0.4f}".format(
+                "Accuracy : {:0.4f}, Precision : {:0.4f}, Recall : {:0.4f}, F-score : {:0.4f}".format(
                     accuracy, precision,
-                    recall, f_score, f1_sc))
+                    recall, f_score))
             
             json.dump(eval_result, open(os.path.join(save_dir, f"epoch_{epoch}_test_result.json"),'w'), indent='\t')
 
-        if val_loss_min > best_loss_min and -1*val_loss_max > best_loss_max:
+        if f_score > best_f1:
             
             if use_wandb:
                 wandb.log(eval_result, step = epoch)
 
-            print(f'At epoch {epoch} New Best score updated from Min_phase: {best_loss_min} Max Phase: {best_loss_max} to Min_phase: {-1*val_loss_min}, Max Phase: {-1*val_loss_max}')
+            print(f'At epoch {epoch} New Best F1 score updated from {best_f1} to {f_score}')
 
-            best_loss_min = val_loss_min
-            best_loss_max = -1*val_loss_max
+            best_f1 = f_score
 
             torch.save(model.state_dict(), os.path.join(save_dir, 'best_model.pt'))
 
